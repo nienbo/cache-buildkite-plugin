045442979b9b70633131e7a75d1d16996e67a83e
Gence W. Genç <gencer@nienbo.co.uk>
2020-08-29 15:37:03 +0300

    fix shellcheck

--- lib/backends/rsync.bash
+++ lib/backends/rsync.bash
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 function restore() {
+  RSYNC_ARGS="--ignore-missing-args"
+  
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    RSYNC_ARGS=""
+  fi
+
   CACHE_PREFIX="${BUILDKITE_PLUGIN_CACHE_RSYNC_STORAGE}/${BUILDKITE_ORGANIZATION_SLUG}/${BUILDKITE_PIPELINE_SLUG}"
 
   mkdir -p "${CACHE_PREFIX}/${CACHE_KEY}"
