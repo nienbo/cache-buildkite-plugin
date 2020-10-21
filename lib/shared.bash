23ddac96a19ffb1cfb0535049285dea018b72374
Gencer W. Genç <gencer@nienbo.co.uk>
2020-10-21 22:20:11 +0300

    add `runner.os` helper

--- lib/shared.bash
+++ lib/shared.bash
@@ -8,7 +8,7 @@
 #   - CACHE_KEY: String that may or may not contain templates.
 # Returns:
 #   - String
-function expand_templates {
+function expand_templates() {
   CACHE_KEY="$1"
   HASHER_BIN="sha1sum"
   if [[ "$OSTYPE" == "darwin"* ]]; then
@@ -21,8 +21,28 @@ function expand_templates {
     if [[ $TEMPLATE_VALUE == "checksum "* ]]; then
       TARGET="$(echo -e "${TEMPLATE_VALUE/"checksum"/""}" | tr -d \' | tr -d \" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
       EXPANDED_VALUE=$(find "$TARGET" -type f -exec $HASHER_BIN {} \; | sort -k 2 | $HASHER_BIN | awk '{print $1}')
+    elif [[ $TEMPLATE_VALUE == "runner.os"* ]]; then
+      if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        OS="Linux"
+      elif [[ "$OSTYPE" == "darwin"* ]]; then
+        OS="macOS"
+      elif [[ "$OSTYPE" == "cygwin" ]]; then
+        OS="Windows"
+      elif [[ "$OSTYPE" == "msys" ]]; then
+        OS="Windows"
+      elif [[ "$OSTYPE" == "win32" ]]; then
+        OS="Windows"
+      elif [[ "$OSTYPE" == "mingw"* ]]; then
+        OS="Windows"
+      elif [[ "$OSTYPE" == "freebsd"* ]]; then
+        OS="Linux" # FreeBSD but still Linux? Should we exclude FreeBSD?
+      else
+        OS="Generic"
+      fi
+      EXPANDED_VALUE="${OS}"
     else
-      >&2 echo "Invalid template expression: $TEMPLATE_VALUE"; return 1
+      echo >&2 "Invalid template expression: $TEMPLATE_VALUE"
+      return 1
     fi
     CACHE_KEY="${BASH_REMATCH[1]}${EXPANDED_VALUE}${BASH_REMATCH[3]}"
   done
@@ -30,19 +50,18 @@ function expand_templates {
   echo "$CACHE_KEY"
 }
 
-
-function cache_hit {
+function cache_hit() {
   echo "🔥 Cache hit: $1"
 }
 
-function cache_restore_skip {
+function cache_restore_skip() {
   echo "🚨 Cache restore is skipped because $1 does not exist"
 }
 
-function source_locating {
+function source_locating() {
   echo "🔍 Locating source: $1"
 }
 
-function cache_locating {
+function cache_locating() {
   echo "🔍 Locating cache: $1"
 }
